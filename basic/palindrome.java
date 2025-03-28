265aaa7e99810fbafcb1cdd69b7b384ecf69ddce
SunitaKumari <172397566+sunitakumari22@users.noreply.github.com>
2025-03-28 17:00:16 +0530

    Updated palindrome.java

--- basic/palindrome.java
+++ basic/palindrome.java
@@ -1,19 +1,27 @@
 import java.util.Scanner;
-
-public class palindrome {
-    public static boolean isPalindrome(int num1) {
-        int originalNum = num1; 
-        int rev = 0;       
+public class palindrome{
+  public static int reverse(int num1) {
+       int originalNum = num1;
+        int rev = 0; int palin=0;
         boolean isNegative = num1 < 0;  
         if (isNegative) {
-            return false; 
+            num1 = -num1;
         }
+
         while (num1 != 0) {
             int digit = num1 % 10;
+            
             rev = rev * 10 + digit;
             num1 = num1 / 10;
         }
-        return rev == originalNum; 
+        if(rev==originalNum){ 
+         palin=1;
+        } 
+
+        else{
+            palin=0;
+        }       
+        return palin;
     }
 
     public static void main(String[] args) {
@@ -21,13 +29,13 @@ public class palindrome {
 
         System.out.print("Enter the number: ");
         int n = scanner.nextInt(); 
-
-        if (isPalindrome(n)) {
-            System.out.println(n + " is a palindrome number.");
-        } else {
-            System.out.println(n + " is not a palindrome number.");
+        int palinn = reverse(n); 
+        if(palinn==1){
+            System.out.println(n + " is  a palindrome number: ");
         }
-
+        else{
+            System.out.println(n + " is not a  palindrome number: ");
+        }
         scanner.close();
     }
 }
